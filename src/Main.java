65c6e2ae3cc6377babfadd59a7f2bd284f8270da
galina <galina.ramodina@gmail.com>
2023-07-28 21:47:08 +0300

    дз 6 первый коммит

---
+++ src/Main.java
@@ -0,0 +1,97 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
+            task4();
+            task5();
+            task6();
+            task7();
+            task8();
+            task9();
+            task10();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2:");
+        for (int i = 10; i>=1; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3:");
+        for (int i = 0; i <= 17; i++) {
+            if (i%2==0) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4:");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5:");
+        for (int i = 1904; i < 2096; i=i+4) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task6() {
+        System.out.println("Задача 6:");
+        for (int i = 7; i <= 98; i = i+7) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task7() {
+        System.out.println("Задача 7:");
+        for (int i = 1; i <= 512; i*=2) {
+            System.out.println(i);
+        }
+    }
+
+    public static void task8() {
+        System.out.println("Задача 8:");
+        int salary = 0;
+        String month;
+        for (int i = 1; i <=12; i++) {
+            salary += 29000;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + salary + " рублей");
+        }
+    }
+
+    public static void task9() {
+        System.out.println("Задача 9:");
+        float salary = 0;
+        String month;
+        for (int i = 1; i <=12; i++) {
+            salary *= 1.1;
+            salary += 29000;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + salary + " рублей");
+        }
+    }
+
+    public static void task10() {
+        System.out.println("Задача 10:");
+        int result;
+        for (int i = 0; i <=10; i++) {
+            result = 2*i;
+            System.out.println("2*" + i + "=" + result);
+        }
+    }
+}
